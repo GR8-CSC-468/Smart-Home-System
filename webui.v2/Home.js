960d7c88b4b972d0b89de7e2dcaa14df79853c9c
cndlovu <124207711+cndlovu@users.noreply.github.com>
2024-04-11 08:18:17 -0400

    Create Home.js

---
+++ webui.v2/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="home-container page-transition">
+      <h1>Welcome to SHMS</h1>
+      <div className="button-container">
+        <Link to="/login" className="button">Login</Link>
+        <Link to="/signup" className="button">Signup</Link>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
