f9e861036a0f17bfcb21bcd77adec91859073341
cndlovu <124207711+cndlovu@users.noreply.github.com>
2024-04-11 09:19:26 -0400

    Update App.js

--- webui.v2/App.js
+++ webui.v2/App.js
@@ -1,21 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import Dashboard from './components/Dashboard';
-import './App.css'; // Import custom CSS file for styling
 
 function App() {
   return (
     <Router>
       <div className="app-container">
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/login" component={Login} />
-          <Route path="/signup" component={Signup} />
-          <Route path="/dashboard" component={Dashboard} />
-        </Switch>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
       </div>
     </Router>
   );
